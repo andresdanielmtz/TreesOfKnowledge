d9c9b0c30ad1e78fa90694dfea7aeb396135eb11
Andrés Martínez <autummata@gmail.com>
2022-12-08 18:39:28 -0700

    lol

---
+++ plantsOfKnowledge/plantsOfKnowledgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantsOfKnowledgeApp.swift
+//  plantsOfKnowledge
+//
+//  Created by Andres Daniel  on 08/12/22.
+//
+
+import SwiftUI
+
+@main
+struct plantsOfKnowledgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
